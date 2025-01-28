42c966a450c2976cd849df8a24e31c6de7bee90c
hsmygit <myhskim79@gmail.com>
2025-01-29 01:08:21 +0900

    :hammer: Feat[#23] 친구 삭제, 친구 요청 API 수정

--- src/main/java/Umc/replendar/friend/repository/FriendRepository.java
+++ src/main/java/Umc/replendar/friend/repository/FriendRepository.java
@@ -1,6 +1,7 @@
 package Umc.replendar.friend.repository;
 
 import Umc.replendar.friend.entity.Friend;
+import Umc.replendar.friend.entity.FriendRequest;
 import Umc.replendar.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -20,5 +21,7 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     // 특정 사용자와 친구가 이미 친구인지 확인
     boolean existsByUserAndFriend(User user, User friend);
+
+
 
 }
