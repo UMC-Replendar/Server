78b13be2c28d473fc6fa837aa02bc5e0ba0976c9
qbnyp <moneycloudy@naver.com>
2025-01-30 19:29:22 +0900

    :sparkles: 카카오 로그인 수정(redirect_uri 추가)

--- src/main/java/Umc/replendar/user/controller/KakaoLoginController.java
+++ src/main/java/Umc/replendar/user/controller/KakaoLoginController.java
@@ -1,18 +1,18 @@
 package Umc.replendar.user.controller;
 
 import Umc.replendar.apiPayload.ApiResponse;
+import Umc.replendar.common.security.JwtTokenProvider;
 import Umc.replendar.user.dto.res.KakaoUserInfoResponseDto;
 import Umc.replendar.user.dto.res.UserDtoRes;
+import Umc.replendar.user.entity.User;
+import Umc.replendar.user.repository.UserRepository;
 import Umc.replendar.user.service.KakaoService;
 import Umc.replendar.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -24,6 +24,8 @@ public class KakaoLoginController {
 
     private final KakaoService kakaoService;
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
 
     @GetMapping("/callback")
     public ApiResponse<UserDtoRes.UserLoginRes> callback(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -39,5 +41,14 @@ public class KakaoLoginController {
     public ApiResponse<String> callback(@RequestParam("code") String code) {
         //회원가입, 로그인 동시진행
         return ApiResponse.onSuccess(code);
+    }
+
+    @PostMapping("whoami")
+    public Long whoami() {
+        Long userIdFromToken = jwtTokenProvider.getUserIdFromToken();
+
+        User user = userRepository.findById(userIdFromToken).orElseThrow(() -> new IllegalArgumentException("유저를 찾지 못했습니다."));
+        //회원가입, 로그인 동시진행
+        return user.getId();
     }
 }
