c8a3bb32bf410ae78e94d6bdbd9b08e9cf0e2ee8
hsmygit <myhskim79@gmail.com>
2025-02-05 09:20:29 +0900

    :sparkles: Feat [#48] 프로필 이미지 변경 api 작성

--- src/main/java/Umc/replendar/user/controller/S3Controller.java
+++ src/main/java/Umc/replendar/user/controller/S3Controller.java
@@ -1,7 +1,9 @@
 package Umc.replendar.user.controller;
 
 import Umc.replendar.apiPayload.ApiResponse;
+import Umc.replendar.common.security.JwtTokenProvider;
 import Umc.replendar.global.util.AmazonS3Util;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,7 @@ import java.util.List;
 public class S3Controller {
 
     private final AmazonS3Util amazonS3Util;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping(value = "/upload/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponse<List<String>> createProfileImage(@PathVariable("id") Long id, @RequestPart(required = false) MultipartFile[] files) throws IOException {
@@ -27,5 +30,14 @@ public class S3Controller {
         }
 
         return ApiResponse.onSuccess(uploadedUrls);
+    }
+    @Operation(summary = "프로필 이미지 변경 API", description = "프로필 이미지를 변경 합니다.")
+    @PostMapping(value = "/update-profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<String> updateProfileImage(@RequestPart("profileImage") MultipartFile profileImage) throws IOException {
+        Long userId = jwtTokenProvider.getUserIdFromToken();
+
+        String imageUrl = amazonS3Util.profileImageUpload(profileImage, userId);
+
+        return ApiResponse.onSuccess(imageUrl);
     }
 }
