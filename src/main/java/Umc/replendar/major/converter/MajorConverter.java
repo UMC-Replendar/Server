a41c86baf98c41b6ba59902c89648683aa232d60
qbnyp <moneycloudy@naver.com>
2025-02-07 17:40:32 +0900

    :sparkles: [#66] 본인 학과의 학년별 과제 조회 API 수정(프론트에서 받기 쉽게 MM/dd 방식으로 포맷)

--- src/main/java/Umc/replendar/major/converter/MajorConverter.java
+++ src/main/java/Umc/replendar/major/converter/MajorConverter.java
@@ -4,6 +4,8 @@ import Umc.replendar.activitylog.entity.Check;
 import Umc.replendar.major.dto.res.LectureAssignmentRes;
 import Umc.replendar.major.entity.LectureAssignment;
 
+import java.time.format.DateTimeFormatter;
+
 public class MajorConverter {
     public static LectureAssignmentRes.LectureAssignmentGetRes toLectureAssignmentGetRes(LectureAssignment lectureAssignment, boolean check) {
         return LectureAssignmentRes.LectureAssignmentGetRes.builder()
@@ -12,7 +14,7 @@ public class MajorConverter {
                 .professor(lectureAssignment.getLecture().getProfessor())
                 .lectureName(lectureAssignment.getLecture().getLectureName())
                 .academicYear(lectureAssignment.getLecture().getAcademicYear())
-                .created_date(lectureAssignment.getCreatedAt().toString())
+                .created_date(lectureAssignment.getCreatedAt().format(DateTimeFormatter.ofPattern("MM/dd")))
                 .due_date(lectureAssignment.getDueDate().toString())
                 .check(check ? Check.CHECK : Check.UNCHECK)
                 .build();
