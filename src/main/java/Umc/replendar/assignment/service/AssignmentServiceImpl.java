7c50ad500b8f4901370b804456c281ee9877130b
qbnyp <moneycloudy@naver.com>
2025-02-20 16:18:14 +0900

    :hammer: Refactor 과제 수정 시 notifyCycle에 값 등록하도록 수정

--- src/main/java/Umc/replendar/assignment/service/AssignmentServiceImpl.java
+++ src/main/java/Umc/replendar/assignment/service/AssignmentServiceImpl.java
@@ -206,6 +206,19 @@ public class AssignmentServiceImpl implements AssignmentService {
         assignment.setFavorite(reqDto.getFavorite());
         assignmentRepository.save(assignment);
 
+        //완료시간에서 알림주기를 계산한 후
+        //과제 알림 주기 테이블에 시간과 알림주기를 저장
+        assNotifyCycleRepository.deleteAllByAssignment(assignment);
+        for(NotifyCycle notifyCycle : reqDto.getNotifyCycle()){
+            AssNotifyCycle assNotifyCycle = AssNotifyCycle.builder()
+                    .assignment(assignment)
+                    .notifyCycle(notifyCycle)
+                    .scheduledAt(TaskTimer.notifyCycle(assignment.getDueDate(), notifyCycle))
+                    .notifyCheck(GeneralSettings.OFF)
+                    .build();
+            assNotifyCycleRepository.save(assNotifyCycle);
+        }
+
         //수정 시 공유할 친구 삭제는 불가능함
         //추가만 가능하기에 이전에 공유했던 친구에게 공유가 중첩되면 안되기에 중첩되는 사람을 필터 후 새로운 사람에게만 과제 공유
         reqDto.getShareIds().removeAll(
